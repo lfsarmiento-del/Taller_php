079a2dbfdf5aeb77f00af6cd59b009f1121218c5
lfsarmiento-del <lfsarmiento@uniboyaca.edu.co>
2026-03-28 12:35:09 -0500

    Correct stylesheet filename in acronimo.php
    Fixed typo in stylesheet link.

--- Taller_php/vistas/acronimo.php
+++ Taller_php/vistas/acronimo.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Conversor de Acrónimos</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/styless.css">
 </head>
 <body>
     <div class="container">
