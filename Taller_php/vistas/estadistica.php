47b6651c14071082d9c9daa625313ef063b5c65e
lfsarmiento-del <lfsarmiento@uniboyaca.edu.co>
2026-03-28 12:36:54 -0500

    Fix HTML structure and update stylesheet link

--- Taller_php/vistas/estadistica.php
+++ Taller_php/vistas/estadistica.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Estadísticas</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/styless.css">
 </head>
 <body>
     <div class="container">
