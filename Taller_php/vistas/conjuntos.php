514b415b5ddf466385c87c080f5d84af698dbc3b
lfsarmiento-del <lfsarmiento@uniboyaca.edu.co>
2026-03-28 12:36:41 -0500

    Fix HTML escaping and update styles.css link

--- Taller_php/vistas/conjuntos.php
+++ Taller_php/vistas/conjuntos.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Operaciones con Conjuntos</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/styless.css">
 </head>
 <body>
     <div class="container">
